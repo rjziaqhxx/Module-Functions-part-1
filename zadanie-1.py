265dba09df7671609367ed95e8803ebc18f26bc9
rjziaqhxx <hjcret11@gmail.com>
2024-10-04 09:04:56 +0200

    zad1

---
+++ zadanie-1.py
@@ -0,0 +1,7 @@
+def Bill():
+    print("""Don't compare yourself with anyone in this world…
+        if you do so, you are insulting yourself."
+                                                        Bill Gates""")
+    
+
+Bill()
